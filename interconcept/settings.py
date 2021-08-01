7434faf71bb47c78813eb0d0e9411530a05596bf
ojoilesanmi <ojoilesanmi89@gmail.com>
2021-08-01 22:47:15 +0100

    Added Email Setup

--- interconcept/settings.py
+++ interconcept/settings.py
@@ -139,5 +139,11 @@ MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
 
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_USE_TLS = config(EMAIL_USE_TLS, cast=bool)
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
