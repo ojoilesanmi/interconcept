6b61c8b5ac4cd933e81d777e9c569416efd92b81
ojoilesanmi <ojoilesanmi89@gmail.com>
2021-08-01 22:14:42 +0100

    Secured SECURITY_KEY and added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn interconcept.wsgi --log-file -
--- interconcept/settings.py
+++ interconcept/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ezog9r3+0f6177*^%n$6obru(j2zb24_+pv2)6sl==wxw#i!5b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
