b6afc179b72e0d69e13aa5c95e001a8252010625
ojoilesanmi <ojoilesanmi89@gmail.com>
2021-08-02 12:49:41 +0100

    Fixed EMAIL_USE_TLS error

--- interconcept/settings.py
+++ interconcept/settings.py
@@ -140,7 +140,7 @@ MESSAGE_TAGS = {
 }
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_USE_TLS = config(EMAIL_USE_TLS, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
