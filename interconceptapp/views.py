6fa1f78768262a946b90e6142a83897080449943
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-08-06 00:03:31 +0100

    Contact page is saving to database

--- interconceptapp/views.py
+++ interconceptapp/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
 
 def index(request):
     return render(request, 'interconcept/index.html')
 
 def contact(request):
     if request.method == "POST":
-        fullname = request.POST["fullname"]
-        email = request.POST["email"]
-        message = request.POST["message"]
+        fullname = request.POST['fullname']
+        email = request.POST['email']
+        message = request.POST['message']
 
-        contact = Conntact(fullname=fullname, email=email, message=message)
+        contact = Contact(fullname=fullname, email=email, message=message)
         contact.save()
-        messages.success("Thank you for your message. We will be in touch shortly!")
-        return redirect('index')
+        messages.success(request, "Thank you for your message. We will be in touch shortly!")
+        return redirect('contact')
     else:
         return render(request, 'interconcept/contact.html')
