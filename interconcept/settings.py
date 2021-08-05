37a9caccfce1a0755394b673e8f09cfe396cbd3f
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-08-05 21:53:17 +0100

    Added whitenoise to settings.py

--- interconcept/settings.py
+++ interconcept/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['interconceptgroup.herokuapp.com']
+ALLOWED_HOSTS = ['interconceptgroup.herokuapp.com', 'localhost']
 
 
 # Application definition
