7bf461e075ffa5904d4b3bceb8def17c7c856dd1
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-09-09 08:46:39 +0100

    Made adjustments to services.html

--- interconceptapp/urls.py
+++ interconceptapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('team', views.team, name='team'),
-    path('about', views.about, name='name'),
+    path('about', views.about, name='about'),
     path('services', views.services, name='services')
 
 ]
