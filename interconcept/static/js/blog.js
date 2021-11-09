8f04db5fe31e73ac1b565c52b5b51da4c4323151
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-11-09 11:14:26 +0100

    Added original text of the company on index.html

--- interconcept/static/js/blog.js
+++ interconcept/static/js/blog.js
@@ -1,65 +1,40 @@
 $(function () {
-    var mediumPromise = new Promise(function (resolve) {
-    var $content = $('#jsonContent');
-    var data = {
-        rss: 'https://medium.com/feed/@interconceptgroups'
-    };
-    $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40interconceptgroups', data, function (response) {
-        if (response.status == 'ok') {
-            $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
-            var display = '';
-            $.each(response.items, function (k, item) {
-                display += `<div class="card mb-3 mx-auto mr-5 " style="width: 20rem;">`;
-                var src = item["thumbnail"]; // use thumbnail url
-                display += `<img src="${src}" class="card-img-top" alt="Cover image">`;
-                display += `<div class="card-body">`;
-                display += `<h5 class="card-title"><a href="${item.link}">${item.title}</a></h5>`;
-                var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-                yourString = yourString.replace('h4', 'p');
-                yourString = yourString.replace('h3', 'p');
-                var maxLength = 120; // maximum number of characters to extract
-                //trim the string to the maximum length
-                var trimmedString = yourString.substr(0, maxLength);
-                //re-trim if we are in the middle of a word
-                trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-                display += `<p class="card-text">${trimmedString}...</p>`;
-                
-                display += `<a href="${item.link}" target="_blank" class="btn btn-outline-success" >Read More</a>`;
-                display += '</div></div>';
-                return k < 10;
-            });
-
-            resolve($content.html(display));
-        }
-    });
-    });
-
-mediumPromise.then(function()
-    {
-        //Pagination
-        pageSize = 4;
-
-        var pageCount = $(".card").length / pageSize;
-
-        for (var i = 0; i < pageCount; i++) {
-            $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
-        }
-        $("#pagin li:nth-child(1)").addClass("active");
-        showPage = function (page) {
-            $(".card").hide();
-            $(".card").each(function (n) {
-                if (n >= pageSize * (page - 1) && n < pageSize * page)
-                    $(this).show();
-            });
-        }
-
-        showPage(1);
-
-        $("#pagin li").click(function () {
-            $("#pagin li").removeClass("active");
-            $(this).addClass("active");
-            showPage(parseInt($(this).text()))
-            return false;
-        });
-    });
+	var $content = $('#jsonContent');
+	var data = {
+		rss_url: 'http://medium.jasonmdesign.com/feed'
+	};
+	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+		if (response.status == 'ok') {
+			var output = '';
+			$.each(response.items, function (k, item) {
+				var visibleSm;
+				if(k < 3){
+					visibleSm = '';
+				 } else {
+					 visibleSm = ' visible-sm';
+				 }
+				output += '<div class="col-sm-6 col-md-4' + visibleSm + '">';
+				output += '<div class="blog-post"><header>';
+				output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";
+				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
+				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
+				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
+				output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></header>';
+				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
+				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
+				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+				var maxLength = 120 // maximum number of characters to extract
+				//trim the string to the maximum length
+				var trimmedString = yourString.substr(0, maxLength);
+				//re-trim if we are in the middle of a word
+				trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+				output += '<p>' + trimmedString + '...</p>';
+				output += '</div></div></div>';
+				return k < 3;
+			});
+			$content.html(output);
+		}
+	});
 });
